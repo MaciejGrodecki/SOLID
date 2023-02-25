09452a33a727d7998f488ed55b8cd3056bc9e383
Maciej Gródecki <maciekg1989@gmail.com>
2023-02-25 13:18:25 +0100

    Implementation which brokes Single Reposibillity Principle

---
+++ Single Responsibility Principle/AddUserService.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SqlClient;
+using System.Net.Mail;
+
+namespace Single_Responsibility_Principle
+{
+    public class AddUserService
+    {
+        public void AddUser()
+        {
+            using var connection = new SqlConnection("connectionString");
+
+            var command = new SqlCommand("INSERT INTO [...]"); // add user to db
+            command.ExecuteNonQuery();
+
+            // sending email
+            var smtpClient = new SmtpClient();
+            smtpClient.Send(new MailMessage());
+        }
+    }
+}
