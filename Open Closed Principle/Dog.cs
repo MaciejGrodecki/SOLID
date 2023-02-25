c6ba54e4461d1fd649d04e5ac9f7dc11b3e85ca0
Maciej Gródecki <maciekg1989@gmail.com>
2023-02-25 13:38:49 +0100

    Wrong Open-closed principle example

---
+++ Open Closed Principle/Dog.cs
@@ -0,0 +1,10 @@
+﻿namespace Open_Closed_Principle
+{
+    public class Dog
+    {
+        public void Say()
+        {
+            Console.WriteLine("Hau Hau");
+        }
+    }
+}
---
+++ Open Closed Principle/Pets.cs
@@ -0,0 +1,23 @@
+﻿namespace Open_Closed_Principle
+{
+    public class Pets
+    {
+        public void SayHello(object[] pets)
+        {
+            foreach (var pet in pets)
+            {
+                if (pet is Cat)
+                {
+                    var cat = (Cat)pet;
+                    cat.Say();
+                }
+
+                if (pet is Dog)
+                {
+                    var dog = (Dog)pet;
+                    dog.Say();
+                }
+            }
+        }
+    }
+}
